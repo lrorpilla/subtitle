d9b1714ef86c487b5fd5e39edd1c599e8752baaf
lrorpilla <leorafaelorpilla@gmail.com>
2021-11-14 16:17:56 +1000

    Increase iterations for duplicate check

--- lib/src/utils/subtitle_controller.dart
+++ lib/src/utils/subtitle_controller.dart
@@ -68,8 +68,9 @@ abstract class ISubtitleController {
       }
     }
 
-    // Attempt this recombination a lot to stamp out persistent values
-    for (var i = 0; i < 3; i++) {
+    // Attempt this recombination a lot to stamp out stubborn repeats
+    // This is not the best way to do this
+    for (var i = 0; i < 10; i++) {
       // Recombine subtitles if they are the same value and next to each other
       for (var i = 1; i < subtitleList.length; i++) {
         var previousSubtitle = subtitleList[i - 1];
